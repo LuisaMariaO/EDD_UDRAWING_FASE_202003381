54f3e60616f0a2bfee5166da591965777ebc3d25
Luisa María Ortíz Romero <luisamaria.ortiz17@gmail.com>
2022-03-13 21:25:42 -0600

    Diseño de login

---
+++ Fase2/src/app/Main.java
@@ -0,0 +1,20 @@
+
+package app;
+import interfaz.Login;
+
+/**
+ *
+ * @author Luisa María Ortiz
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOLA");
+        Login login = new Login();
+        login.setVisible(true);
+    }
+    
+}
